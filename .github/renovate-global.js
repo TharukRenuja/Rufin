c6d888cce642541d01659c4902e25ec7d1fa50ad
screwys <254296947+screwys@users.noreply.github.com>
2026-05-27 18:46:53 +0300

    ci(renovate): add self-hosted workflow

---
+++ .github/renovate-global.js
@@ -0,0 +1,8 @@
+module.exports = {
+  platform: "github",
+  repositories: ["screwys/Rufin"],
+  onboarding: false,
+  requireConfig: "required",
+  ignorePrAuthor: true,
+  allowedCommands: ["^bash packaging/flatpak/update-cargo-sources\\.sh$"],
+};
